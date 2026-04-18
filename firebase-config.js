81ab6c0a7fcd422b702135d8f3b1e379c04c617a
Preetham.M <preetham.m0099@gmail.com>
2026-04-18 23:47:43 +0530

    Fix Firebase config module for auth

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBVrm9zr6bS8DP5dIw67ItMdYuFgMRGj0w",
   authDomain: "workbook-abb8e.firebaseapp.com",
   projectId: "workbook-abb8e",
   storageBucket: "workbook-abb8e.firebasestorage.app",
   messagingSenderId: "462434137551",
   appId: "1:462434137551:web:ef6b2906a1b429ce634eba",
-  measurementId: "G-NDK9E6M53C"
+  measurementId: "G-NDK9E6M53C",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export function isFirebaseConfigured(config) {
+  return Boolean(
+    config?.apiKey &&
+      config?.authDomain &&
+      config?.projectId &&
+      config?.appId &&
+      !config.apiKey.includes("YOUR_"),
+  );
+}
